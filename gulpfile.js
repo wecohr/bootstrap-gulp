96d7d56387068e889f6026e9fba2d2442d549009
Unknown <weco8.ri@gmail.com>
2017-01-20 14:45:24 +0100

    gulpfile.js opisi nedovršeni

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
+    //Inicjalizacija gulp.js i ostalih pluginova
     var gulp = require('gulp');
     var sass = require('gulp-sass');
 
+//Inicjalizacija direktorija
     var dir = {
-      bootstrapsass: '././bower_components/bootstrap-sass',
-      production: '././production',
+      bootstrapsass: '././bower_components/bootstrap-sass', //dir gdje se nalazi bootstrap sass
+      production: '././production', //dir za produkciju
     };
+//dir.production oznacuje direktorij
 
+//gulp task za stvaranje css-a iz sass-a
     gulp.task('css', function(){
-        return gulp.src('./development/sass/style.scss')
+        return gulp.src('./development/sass/style.scss') //??
         .pipe(sass({
-          outputStyle:'compressed',
-          includePaths: [dir.bootstrapsass + '/assets/stylesheets'],
+          outputStyle:'compressed', //kompresiran css
+          includePaths: [dir.bootstrapsass + '/assets/stylesheets'], //ucitava default stylesheets
         }))
-      .pipe(gulp.dest(dir.production + '/css'));
+      .pipe(gulp.dest(dir.production + '/css')); //output u produkciju kao style.css
     });
 
+//kopiranje fontova u produkciju
 gulp.task('fonts', function(){
   return gulp.src(dir.bootstrapsass + '/assets/fonts/**/*')
   .pipe(gulp.dest(dir.production + '/fonts'));
 });
 
+//defalut task za pokretanje gulp-a
 gulp.task('default', ['css', 'fonts']);
