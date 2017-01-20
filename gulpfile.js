e2eff48dfb2ef80a9c8153370b4568b2b46f22a0
Unknown <weco8.ri@gmail.com>
2017-01-20 12:33:47 +0100

    Uspješno radi gulp - kompajliranje sassa i fonta

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,22 @@
-    var gulp = require('gulp'),
+    var gulp = require('gulp');
     var sass = require('gulp-sass');
+
+    var dir = {
+      bootstrapsass: '././bower_components/bootstrap-sass',
+      production: '././production',
+    };
+
+    gulp.task('css', function(){
+        return gulp.src('./development/sass/style.scss')
+        .pipe(sass({
+          includePaths: [dir.bootstrapsass + '/assets/stylesheets'],
+        }))
+      .pipe(gulp.dest(dir.production + '/css'));
+    });
+
+gulp.task('fonts', function(){
+  return gulp.src(dir.bootstrapsass + '/assets/fonts/**/*')
+  .pipe(gulp.dest(dir.production + '/fonts'));
+});
+
+gulp.task('default', ['css', 'fonts']);
