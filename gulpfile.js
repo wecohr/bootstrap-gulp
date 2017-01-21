f9bd3a40ae4d83c134b9ab59694ee34e11566429
Unknown <weco8.ri@gmail.com>
2017-01-21 10:35:33 +0100

    Posložen gulpfile.js + watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
-    //Inicjalizacija gulp.js i ostalih pluginova
+//-----------------------------------------INICJALIZACIJA GULP PLUGINOVA----------------------------------------------------->
     var gulp = require('gulp'),
         sass = require('gulp-sass'),
         cssscss = require('gulp-css-scss'),
         rename = require('gulp-rename');
+//------------------------------------------------------------------------------------------------------------------------------>
 
-//Inicjalizacija direktorija
+//--------------------------INICJALIZACIJA VARIJABLI ZA DIREKTORIJE------------------------------------------------------------->
     var dir = {
       development: '././development', //dir za temp
       bootstrapsass: '././bower_components/bootstrap-sass', //dir gdje se nalazi bootstrap sass
       production: '././production', //dir za produkciju
     };
-//dir.production oznacuje direktorij
+//------------------------------------------------------------------------------------------------------------------------------>
 
+//--------------------------------------------SASS/CSS-------------------------------------------------------------------------->
+    //BROJ JEDAN
+    //pg-custom.css ---> pg-custom.scss
+    gulp.task('customcss', function(){
+       gulp.src(dir.development + '/css/pg-custom.css')
+      .pipe(cssscss())
+      .pipe(gulp.dest('development/sass/'));
+    });
 
+//BROJ DVA
 //Stvaranje style.css - BOOTSTRAP & CUSTOM
     gulp.task('sass', function(){
         return gulp.src('./development/sass/style.scss') //temeljni scss gdje se sve kompajlira
@@ -23,21 +33,22 @@
       .pipe(gulp.dest(dir.production + '/css')); //output u produkciju kao style.css
     });
 
+    //Task za stvaranje style.css
+    gulp.task('css', ['customcss', 'sass']);
+//------------------------------------------------------------------------------------------------------------------------------>
 
-//BROJ DVA
-//pg-custom.css ---> pg-custom.scss
-gulp.task('customcss', function(){
-   gulp.src(dir.development + '/css/pg-custom.css')
-  .pipe(cssscss())
-  .pipe(gulp.dest('development/sass/'));
-
-});
+//--------------------------------------------FONTS--------------------------------------------------------------------------->
 
 //kopiranje fontova u produkciju
 gulp.task('fonts', function(){
   return gulp.src(dir.bootstrapsass + '/assets/fonts/**/*')
   .pipe(gulp.dest(dir.production + '/fonts'));
 });
+//------------------------------------------------------------------------------------------------------------------------------>
 
-//Task za stvaranje style.css
-gulp.task('css', ['customcss', 'sass']);
+
+//-------------------------------------------------WATCH TASK--------------------------------------------------------------------->
+gulp.task('watch', function () {
+  gulp.watch(dir.development + '/css/*.css', ['css']); //prati sve promjene u development/css i pokreće css funkciju za stvaranje style.css
+});
+//------------------------------------------------------------------------------------------------------------------------------>
